c8e89c128192ec07c1e98ff18ec5f2616cce4740
rpeim <rpeim@ipsosenso.com>
2026-09-23 14:21:06 +0200

    Integration tests setup

---
+++ server/src/test/java/com/personal/task_tracker/task/TaskControllerTest.java
@@ -0,0 +1,15 @@
+package com.personal.task_tracker.task;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+class TaskControllerTest {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:16-alpine");}
